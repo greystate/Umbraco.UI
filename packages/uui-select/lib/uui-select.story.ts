be40ff885ba577e22cc705c60471aa6ba606ef7c
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-04-28 11:42:16 +0200

    autoupgrade story to new format

--- packages/uui-select/lib/uui-select.story.ts
+++ packages/uui-select/lib/uui-select.story.ts
@@ -1,6 +1,7 @@
+import './uui-select-option.element';
 import '.';
 
-import { Story } from '@storybook/web-components';
+import { StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 
 const options: Array<Option> = [
@@ -56,122 +57,137 @@ const options: Array<Option> = [
   },
 };
 
-export const AAAOverview: Story = props =>
-  html`<uui-select
-    .options=${options}
-    .placeholder=${props.placeholder}
-    .disabled=${props.disabled}
-    .label=${props.label}
-    ?error=${props.error}></uui-select>`;
-AAAOverview.storyName = 'Overview';
+export const AAAOverview: StoryObj = {
+  render: props =>
+    html`<uui-select
+      .options=${options}
+      .placeholder=${props.placeholder}
+      .disabled=${props.disabled}
+      .label=${props.label}
+      ?error=${props.error}></uui-select>`,
 
-export const Preselected: Story = () =>
-  html`<uui-select
-    .options=${preselectedOptions}
-    label="Preselected"></uui-select>`;
-Preselected.parameters = {
-  docs: {
-    source: {
-      code: `
-<uui-select placeholder="Select an option"></uui-select>
+  name: 'Overview',
+};
 
-//this is an example of array you need to pass to the select component to print the options
-const options: Array<Option> = [
-{ name: 'Carrot', value: 'orange' },
-{ name: 'Cucumber', value: 'green' },
-{ name: 'Aubergine', value: 'purple', selected: true },
-{ name: 'Blueberry', value: 'Blue' },
-{ name: 'Banana', value: 'yellow' },
-{ name: 'Strawberry', value: 'red' },
-];
+export const Preselected: StoryObj = {
+  render: () =>
+    html`<uui-select
+      .options=${preselectedOptions}
+      label="Preselected"></uui-select>`,
 
-`,
+  parameters: {
+    docs: {
+      source: {
+        code: `
+  <uui-select placeholder="Select an option"></uui-select>
+
+  //this is an example of array you need to pass to the select component to print the options
+  const options: Array<Option> = [
+  { name: 'Carrot', value: 'orange' },
+  { name: 'Cucumber', value: 'green' },
+  { name: 'Aubergine', value: 'purple', selected: true },
+  { name: 'Blueberry', value: 'Blue' },
+  { name: 'Banana', value: 'yellow' },
+  { name: 'Strawberry', value: 'red' },
+  ];
+
+  `,
+      },
     },
   },
 };
 
-export const Groups: Story = props =>
-  html`<uui-select
-    .options=${groupedOptions}
-    label="Grouped"
-    .placeholder=${props.placeholder}></uui-select>`;
+export const Groups: StoryObj = {
+  render: props =>
+    html`<uui-select
+      .options=${groupedOptions}
+      label="Grouped"
+      .placeholder=${props.placeholder}></uui-select>`,
 
-Groups.parameters = {
-  controls: { include: ['placeholder'] },
-  docs: {
-    source: {
-      code: `
-<uui-select placeholder="Select an option"></uui-select>
+  parameters: {
+    controls: { include: ['placeholder'] },
+    docs: {
+      source: {
+        code: `
+  <uui-select placeholder="Select an option"></uui-select>
 
-//this is an example of array you need to pass to the select component to print the options
-const options: Array<Option> = [
-{ name: 'Carrot', value: 'orange', group: 'Vegetables' },
-{ name: 'Cucumber', value: 'green', group: 'Vegetables' },
-{ name: 'Aubergine', value: 'purple',, group: 'Vegetables' },
-{ name: 'Blueberry', value: 'Blue', group: 'Fruits' },
-{ name: 'Banana', value: 'yellow', group: 'Fruits' },
-{ name: 'Strawberry', value: 'red', group: 'Fruits' },
-];
+  //this is an example of array you need to pass to the select component to print the options
+  const options: Array<Option> = [
+  { name: 'Carrot', value: 'orange', group: 'Vegetables' },
+  { name: 'Cucumber', value: 'green', group: 'Vegetables' },
+  { name: 'Aubergine', value: 'purple',, group: 'Vegetables' },
+  { name: 'Blueberry', value: 'Blue', group: 'Fruits' },
+  { name: 'Banana', value: 'yellow', group: 'Fruits' },
+  { name: 'Strawberry', value: 'red', group: 'Fruits' },
+  ];
 
-`,
+  `,
+      },
     },
   },
 };
 
-export const DisabledGroups: Story = props =>
-  html`<uui-select
-    .options=${groupedOptions}
-    label="Disabled Group"
-    disabledGroups="vegetables"
-    .placeholder=${props.placeholder}></uui-select>`;
-DisabledGroups.parameters = {
-  controls: { include: ['placeholder'] },
-  docs: {
-    source: {
-      code: `
-<uui-select disabledGroups="vegetables"></uui-select>`,
+export const DisabledGroups: StoryObj = {
+  render: props =>
+    html`<uui-select
+      .options=${groupedOptions}
+      label="Disabled Group"
+      disabledGroups="vegetables"
+      .placeholder=${props.placeholder}></uui-select>`,
+
+  parameters: {
+    controls: { include: ['placeholder'] },
+    docs: {
+      source: {
+        code: `
+  <uui-select disabledGroups="vegetables"></uui-select>`,
+      },
     },
   },
 };
 
-export const Disabled: Story = props =>
-  html`<uui-select
-    .options=${options}
-    label="Label"
-    .placeholder=${props.placeholder}
-    .disabled=${props.disabled}></uui-select> `;
+export const Disabled: StoryObj = {
+  render: props =>
+    html`<uui-select
+      .options=${options}
+      label="Label"
+      .placeholder=${props.placeholder}
+      .disabled=${props.disabled}></uui-select> `,
 
-Disabled.args = {
-  disabled: true,
-};
+  args: {
+    disabled: true,
+  },
 
-Disabled.parameters = {
-  controls: { include: ['disabled'] },
-  docs: {
-    source: {
-      code: `
-<uui-select disabled></uui-select>`,
+  parameters: {
+    controls: { include: ['disabled'] },
+    docs: {
+      source: {
+        code: `
+  <uui-select disabled></uui-select>`,
+      },
     },
   },
 };
 
-export const Error: Story = props =>
-  html`<uui-select
-    .options=${options}
-    label="Label"
-    .placeholder=${props.placeholder}
-    ?error=${props.error}></uui-select>`;
+export const Error: StoryObj = {
+  render: props =>
+    html`<uui-select
+      .options=${options}
+      label="Label"
+      .placeholder=${props.placeholder}
+      ?error=${props.error}></uui-select>`,
 
-Error.args = {
-  error: true,
-};
+  args: {
+    error: true,
+  },
 
-Error.parameters = {
-  controls: { include: ['error'] },
-  docs: {
-    source: {
-      code: `
-<uui-select error></uui-select>`,
+  parameters: {
+    controls: { include: ['error'] },
+    docs: {
+      source: {
+        code: `
+  <uui-select error></uui-select>`,
+      },
     },
   },
 };
